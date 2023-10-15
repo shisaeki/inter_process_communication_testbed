811685d62bba2a98478c6a9e7050cfa8e68c4470
ShintaroSaeki <saeki@mur4l.com>
2023-10-15 14:01:34 +0900

    ADD: execve

--- execve_example/hello.c
+++ execve_example/hello.c
@@ -1,27 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   execve.c                                           :+:      :+:    :+:   */
+/*   hello.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: shinsaeki <shinsaeki@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/15 13:55:08 by shinsaeki         #+#    #+#             */
-/*   Updated: 2023/10/15 14:00:51 by shinsaeki        ###   ########.fr       */
+/*   Created: 2023/10/15 13:57:57 by shinsaeki         #+#    #+#             */
+/*   Updated: 2023/10/15 13:58:18 by shinsaeki        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <unistd.h>
 
 int main()
 {
-    char    *path;
-    char    *argv[] = {"./hello", NULL};
-    char    *env[] = {NULL};
-
-    path = "./hello";
-    execve(path, argv, env);
-
-    printf("failed to execve");
+    printf("Hello...");
     return (0);
 }
