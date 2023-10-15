c971846574b1b4e503cdf904b01700842aa2112f
ShintaroSaeki <saeki@mur4l.com>
2023-10-15 13:54:40 +0900

    ADD: fork folder

--- fork_example/fork.c
+++ fork_example/fork.c
@@ -6,7 +6,7 @@
 /*   By: shinsaeki <shinsaeki@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 13:22:09 by shinsaeki         #+#    #+#             */
-/*   Updated: 2023/10/15 13:24:32 by shinsaeki        ###   ########.fr       */
+/*   Updated: 2023/10/15 13:41:34 by shinsaeki        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,14 @@ int main(void)
     pid_t	pid;
 
 	pid = fork();
-	if (pid == 0)
-		printf("Child Process\n");
+	if (pid < 0)
+	{
+		printf("error fork");
+		return (0);
+	}
+	else if (pid == 0)
+		printf("Child Process ID: %d\n", getpid());
 	else
-		printf("Parent Process\n");
+		printf("Parent Process ID: %d\n", pid);
 	return (0);
 }
